ff9cb3ad36b7f69715b4f8b8f8c7d708bd5c9002
Rob Cerda <16689532+robcerda@users.noreply.github.com>
2023-11-10 14:28:44 -0800

    Update setup_chromeos.sh

--- chromeos/setup_chromeos.sh
+++ chromeos/setup_chromeos.sh
@@ -30,17 +30,18 @@ echo "Created 'Default keyring'."
 seahorse --change-password "Default keyring"
 echo "Set 'Default keyring' as the default keyring."
 
-# Check if the additional packages are installed, and if not, install them non-interactively
-if ! dpkg -l | grep -q nmap; then
-    sudo apt install -y nmap
+# Install Proxychains and Tor
+if ! dpkg -l | grep -q proxychains; then
+    sudo apt install -y proxychains
 fi
 
-if ! dpkg -l | grep -q lynx; then
-    sudo apt install -y lynx
+if ! dpkg -l | grep -q tor; then
+    sudo apt install -y tor
 fi
 
-if ! dpkg -l | grep -q net-tools; then
-    sudo apt install -y net-tools
+# Install Nano text editor
+if ! dpkg -l | grep -q nano; then
+    sudo apt install -y nano
 fi
 
 # Download deb files to the Downloads folder if they don't exist
@@ -50,23 +51,6 @@ fi
 
 if [ ! -f ~/Downloads/vscode.deb ]; then
     wget -O ~/Downloads/vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
-fi
-
-# Install downloaded deb files non-interactively
-if ! dpkg -l | grep -q '1password-latest'; then
-    if sudo DEBIAN_FRONTEND=noninteractive dpkg -i ~/Downloads/1password-latest.deb; then
-        echo "1Password installed successfully."
-    else
-        echo "Error: Failed to install 1Password."
-    fi
-fi
-
-if ! dpkg -l | grep -q 'vscode'; then
-    if sudo DEBIAN_FRONTEND=noninteractive dpkg -i ~/Downloads/vscode.deb; then
-        echo "Visual Studio Code installed successfully."
-    else
-        echo "Error: Failed to install Visual Studio Code."
-    fi
 fi
 
 # Remember to make this script executable before running it
